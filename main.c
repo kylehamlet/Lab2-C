bc4a83908e32a386bebc739e09ce11adf24f824e
Kyle Hamlet <kjh6064@psu.edu>
2020-09-11 04:40:09 +0000

    b

--- main.c
+++ main.c
@@ -16,22 +16,22 @@ char *lettergrade;
   if (93.0 > gradevalue && gradevalue >= 90.0) {
     lettergrade = "A-";
   }
-  if (gradevalue >= 87.0) {
+  if (90.0 > gradevalue && gradevalue >= 87.0) {
     lettergrade = "B+";
   }
-  if (gradevalue >= 83.0) {
+  if (87.0 > gradevalue && gradevalue >= 83.0) {
     lettergrade = "B";
   }
-  if (gradevalue >= 80.0) {
+  if (83.0 > gradevalue && gradevalue >= 80.0) {
     lettergrade = "B-";
   }
-  if (gradevalue >= 77.0) {
+  if (80.0 > gradevalue && gradevalue >= 77.0) {
     lettergrade = "C+";
   }
-  if (gradevalue >= 70.0) {
+  if (77.0 > gradevalue && gradevalue >= 70.0) {
     lettergrade = "C";
   }
-  if (gradevalue >= 60.0) {
+  if (70.0 > gradevalue && gradevalue >= 60.0) {
     lettergrade = "D";
   }
   else if (gradevalue < 60.0) {
